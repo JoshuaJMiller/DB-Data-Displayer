204eb3fc4267fdc860f54a12c378420ce8332f45
JoshuaJMiller <joshua.miller1014@gmail.com>
2021-10-29 18:07:01 -0700

    init commit

---
+++ HelperLibrary/DataAccess.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperLibrary
+{
+    public class DataAccess
+    {
+        public List<PersonModel> GetPeopleByLastName(string p_lastName)
+        {
+            // Makes a new IDbConnection based on the connection string that is returned, and closes the connection as soon as function returns
+            using(IDbConnection connection = new System.Data.SqlClient.SqlConnection(ConnectionHelper.getConnectionString("PersonDB")))
+            {
+                // Dapper Query method that calls stored procedure "People_GetByLastName" from SQL Server which has a parameter "@LastName" p_lastName is passed via inline object initialization (new) 
+                List<PersonModel> output = connection.Query<PersonModel>("dbo.People_GetByLastName @LastName", new { LastName = p_lastName }).ToList();
+                return output;
+            }
+        }
+
+        
+
+    }
+}
---
+++ WPFUI/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using HelperLibrary;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Button clear = new Button();
+        public List<PersonModel> People = new List<PersonModel>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            People.Clear();
+            DataAccess dataAcccess = new DataAccess();
+            People = dataAcccess.GetPeopleByLastName(searchEntry.Text);
+
+            personInfoPanel.Children.Clear();
+            foreach (PersonModel person in People)
+            {
+                TextBlock tb = new TextBlock();
+                tb.Text = person.FullInfo;
+                personInfoPanel.Children.Add(tb);
+            }
+
+            if (personInfoPanel.Children.Count > 0)
+            {
+                clear.Content = "clear";
+                clear.Click += new RoutedEventHandler(clear_Click);
+                searchPanel.Children.Add(clear);
+            }
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            personInfoPanel.Children.Clear();
+            searchPanel.Children.Remove(clear);
+        }
+
+        private void searchEntry_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                searchButton_Click(sender, e);
+            }
+        }
+    }
+}
